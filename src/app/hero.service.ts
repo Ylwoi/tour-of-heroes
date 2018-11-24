8f0db6308de5a3b9b0275a453a4c07eeff7e46a3
Horvath Balazs <balazs.horvath989@gmail.com>
2018-11-24 20:35:11 +0100

    hero-detail component:  Get the route that created it; Extract the id from the route; Acquire the hero with that id from the server via the HeroService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,14 +12,18 @@ import { MessageService } from './message.service';
 export class HeroService {
 
   constructor(private messageService: MessageService) { }
-  
-  // getHeroes(): Hero[] {
-  //   return HEROES;
-  // }
+
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
     return of (HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    //noinspection TypeScriptUnresolvedFunction
+    return of(HEROES.find(hero => hero.id === id));
   }
 
 }
