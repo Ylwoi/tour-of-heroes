7e1bed1925ad80897ac0ad35f97604145f56cce5
Horvath Balazs <balazs.horvath989@gmail.com>
2018-11-24 19:42:18 +0100

    ws error fixed in spec.ts files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import {expect, describe} from "@angular/core/testing/src/testing_internal";
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
+import {expect, describe} from "@angular/core/testing/src/testing_internal";
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
